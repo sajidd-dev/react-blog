c7bdcee9eec2114ac6aa824d1c3980dc16684da5
sajidd-dev <sajid.naseem55541@gmail.com>
2020-08-15 11:48:52 +0500

    move private info in dev.js

--- index.js
+++ index.js
@@ -3,15 +3,19 @@ const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const {User} = require('./model/user')
+const {User} = require('./model/user');
+const config = require('./config/key');
 
-mongoose.connect('mongodb://localhost:27017/react-blog', { useNewUrlParser: true });
+mongoose.connect(config.mongoURI, { useNewUrlParser: true });
 
 mongoose.connection.once('open',function(){
     console.log('connection');
 }).on('error',function(error){
-    
     console.log('connection error:',error);
+})
+
+app.get('/', (req,res) => {
+    res.send('hello world ');
 })
 
 app.use(bodyParser.urlencoded({extended :true}));
@@ -20,7 +24,6 @@ app.use(cookieParser());
 
 app.post('/api/users/register', (req, res) => {
     const user = new User(req.body);
-
     user.save((err, userData) => {
         if(err) return res.json({success: false, err})
         return res.status(200).json({
@@ -30,3 +33,6 @@ app.post('/api/users/register', (req, res) => {
 })
 
 app.listen(5000);
+
+
+
