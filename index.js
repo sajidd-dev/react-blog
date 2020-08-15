cf409917260858cae245169d352ee1a86220945c
sajidd-dev <sajid.naseem55541@gmail.com>
2020-08-15 13:18:16 +0500

    bcrypt:password-login:method-create:token

--- index.js
+++ index.js
@@ -24,12 +24,38 @@ app.use(cookieParser());
 
 app.post('/api/users/register', (req, res) => {
     const user = new User(req.body);
-    user.save((err, userData) => {
+    user.save((err, doc) => {
         if(err) return res.json({success: false, err})
         return res.status(200).json({
-            success : true
+            success : true,
+            userData : doc
         })
     });
+})
+
+app.post('/api/user/login', (req,res) => {
+    User.findOne({email: req.body.email}, (err, user) => {
+        if(!user){
+            return res.json({
+                loginSuccess : false,
+                message : "failed"
+            });
+        }
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if(!isMatch){
+                return res.json({loginSuccess: false, message : 'wrong password'})
+            }
+        })
+
+        user.generateToken((err, user) => {
+            if(err) return res.status(400).send(err);
+            res.cookie("x_auth", user.token).status(200)
+            .json({
+                loginSuccess:true
+            })
+        })
+    })
+
 })
 
 app.listen(5000);
--- model/user.js
+++ model/user.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
-
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const saltRounds = 10;
 const userSchema = mongoose.Schema({
     name : {
         type:String,
@@ -30,7 +32,40 @@ const userSchema = mongoose.Schema({
     }
 })
 
-const User = mongoose.model('User', userSchema)
+    userSchema.pre('save', function( next ){
+        var user = this;
+        if(user.isModified('password')){
+            bcrypt.genSalt(saltRounds, function(err, salt){
+                if(err) return next(err);
+                bcrypt.hash(user.password, salt, function(err,hash){
+                if(err) return next(err);
+                user.password = hash
+                next()
+                })
+            })
+        }else{
+            next ()
+        }     
+    });
+
+userSchema.methods.comparePassword = function(plainPassword,cb){
+    bcrypt.compare(plainPassword,this.password,function(err,isMatch){
+        if(err) return cb(err);
+        cb(null,isMatch)
+    })
+}
+
+userSchema.methods.generateToken = function(cb){
+    var user = this;
+    var token = jwt.sign(user._id.toHexString(),'secret')
+    user.token = token;
+    user.save(function(err, user) {
+        if(err) return cb(err)
+        cb(null,user);
+    })
+}
+
+const User = mongoose.model('User', userSchema);
 
 module.exports = { User }
 
