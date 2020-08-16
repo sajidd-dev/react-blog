65923872fc0143f390de5adf17f280f4df9cf032
sajidd-dev <sajid.naseem55541@gmail.com>
2020-08-16 18:21:07 +0500

    auth

--- model/user.js
+++ model/user.js
@@ -65,13 +65,13 @@ userSchema.methods.generateToken = function(cb){
     })
 }
 
-userSchema.statics.findByToken = function(token , cb){
+userSchema.statics.findByToken = function (token, cb) {
     var user = this;
 
-    jwt.verify(token,'secret', function(err,decode){
-        user.findOne({"_id":decode,"token":token},function( err, cb){
+    jwt.verify(token,'secret',function(err, decode){
+        user.findOne({"_id":decode, "token":token}, function(err, user){
             if(err) return cb(err);
-            cb(null,user);
+            cb(null, user);
         })
     })
 }
